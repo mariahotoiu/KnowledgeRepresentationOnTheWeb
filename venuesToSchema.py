701146680602152d3656a68371b0a0625b7d89ee
mariahotoiu <mariahotoiu@yahoo.com>
2018-04-17 15:31:09 +0200

    complete transformation of yelp with schema vocab

--- venuesToSchema.py
+++ venuesToSchema.py
@@ -13,7 +13,14 @@ root = 'D:\Cursuri Master\Knowledge Representation on the Web\Yelp'
 with open(os.path.join(root,'venuesYelp.json')) as json_data:
     response = json.load(json_data)
 
-# create an RDF graph
+root2 = 'D:\Cursuri Master\Knowledge Representation on the Web'
+with open(os.path.join(root2,'schemaTypes.txt'), "r") as word_list:
+    schemaTypes = word_list.read().split('\n')
+
+with open(os.path.join(root2,'schemaSubtypes.txt'), "r") as word_list:
+    schemaSubTypes = word_list.read().split('\n')
+
+
 g = Graph()
 
 venue = BNode()
@@ -22,18 +29,45 @@ schema = Namespace("http://schema.org/")
 
 g.bind('schema',schema)
 
-
-g.add((venue, RDF.type, schema.localBusiness))
-
+categories = [a.strip() for a in response[1]['CategoriesYelp'].split(',')]
+#
+#
+cat = 0
+for category in categories:
+    category = category.replace(" ","")
+    category = category.replace("&","And")
+    if category.endswith("s"):
+        category = category[:-1]
+    st = [s for s in schemaSubTypes if category.lower() in s.lower()]
+    t = [s for s in schemaTypes if category.lower() in s.lower()]
+    if st != [] :
+        g.add((venue, RDF.type, URIRef(schema + st[0])))
+        if cat == 1:
+            g.remove((venue, RDF.type, schema.localBusiness))
+        else:
+            cat = 1
+    elif t != []:
+        g.add((venue, RDF.type, URIRef(schema + t[0])))
+        if cat == 1:
+            g.remove((venue, RDF.type, schema.localBusiness))
+        else:
+            cat = 1
+    else:
+        if cat == 0:
+            g.add((venue, RDF.type, schema.localBusiness))
+            addProp = BNode()
+            g.add((venue,schema.additionalProperty, addProp))
+            g.add((addProp, schema.name, Literal('category')))
+            g.add((addProp, schema.value, Literal(category)))
+            cat = 1
+        else:       
+            addProp = BNode()
+            g.add((venue,schema.additionalProperty, addProp))
+            g.add((addProp, schema.name, Literal('category')))
+            g.add((addProp, schema.value, Literal(category)))
 
 g.add((venue, schema.name, Literal(response[1]['Name'], lang='en')))
 
-
-
-#categories = [a.strip() for a in response[1]['CategoriesYelp'].split(',')]
-#
-#
-#for category in categories:
 #    g.add((venue_URI, ['categoryYelp'], Literal(category)))
 
 address = BNode() 
@@ -43,17 +77,25 @@ g.add((address, schema.addressLocalitly, Literal(response[1]['City'], lang='en')
 g.add((address, schema.streetAddress,Literal(response[1]['Address'],lang='en')))
 g.add((address, schema.postalCode, Literal(response[1]['PostalCode'])))
 g.add((address, schema.addressRegion, Literal(response[1]['State'])))
+
 geo = BNode()
 g.add((venue,schema.geo,geo))
 g.add((geo, RDF.type, schema.GeoCoordinates))
 g.add((geo,schema.latitude,Literal(response[1]['Latitude'], datatype=XSD.decimal)))
 g.add((geo,schema.longitude,Literal(response[1]['Longitude'], datatype=XSD.decimal)))
+
 rating = BNode()
 g.add((venue, schema.aggregateRating, rating))
-g.add((rating, RDF.type, schema.aggregateRating))
+g.add((rating, RDF.type, schema.AggregateRating))
 g.add((rating, schema.ratingValue,Literal(response[1]['RatingYelp'], datatype=XSD.decimal)))
 g.add((rating, schema.reviewCount, Literal(response[1]['ReviewCountYelp'],datatype = XSD.integer)))
 g.add((rating, schema.author, Literal('Yelp')))
+
+identifier = BNode()
+g.add((venue, schema.identifier, identifier))
+g.add((identifier, RDF.type, schema.PropertyValue))
+g.add((identifier, schema.propertyID, Literal('BusinessIdYelp')))
+g.add((identifier, schema.value,Literal(response[1]['BusinessIdYelp'])))
 
 print g.serialize(format='turtle')
 
