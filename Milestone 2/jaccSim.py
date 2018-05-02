034bb9c3e64cc724270bc53c3da9d4d2d92450e6
mariahotoiu <mariahotoiu@yahoo.com>
2018-05-02 11:48:18 +0200

    function now gets responses for all pages
    if run with 'while' the function takes a lot of time to get the responses from all pages, so for testing run with 'if'

--- Milestone 2/jaccSim.py
+++ Milestone 2/jaccSim.py
@@ -11,19 +11,32 @@ def JaccSim(c1,c2):
     ('p', 'rdf:type'),
     ('o', c1),
     )
-
-    response1 = requests.get('https://hdt.lod.labs.vu.nl/triple', params=params1)
-    response1 = response1.text.split(' ')
-    print response1[0]
+    
+    r = requests.get('https://hdt.lod.labs.vu.nl/triple', params=params1)
+    response1 = r.text
+#    while 'next' in r.links.keys():  
+    if 'next' in r.links.keys():    
+        r = requests.get(r.links['next']['url'], params = params1)  
+        response1 += r.text
+    response1 = response1.split(' ')
+    response1 = response1[:-1]
+#    print len(response1)
     
     params2 = (
     ('p', 'rdf:type'),
     ('o', c2),
     )
-                                
-    response2 = requests.get('https://hdt.lod.labs.vu.nl/triple', params=params2)
-    response2 = response2.text.split(' ')
-    print response2[0]
+                             
+    r = requests.get('https://hdt.lod.labs.vu.nl/triple', params=params2)
+    response2 = r.text
+#    while 'next' in r.links.keys():
+    if 'next' in r.links.keys():
+        r = requests.get(r.links['next']['url'], params = params1)  
+        response2 += r.text
+    
+    response2 = response2.split(' ')
+    response2 = response2[:-1]
+#    print response2[0]
     
     for i in range(4,len(response1),4):
         response1[i] = response1[i][2:]
@@ -34,21 +47,21 @@ def JaccSim(c1,c2):
         response2[i] = response2[i][2:]
         
     
-    objects1 = [response1[i] for i in range(0,len(response1),4)]      
-    objects2 = [response2[i] for i in range(0,len(response2),4)]  
+    subjects1 = [response1[i] for i in range(0,len(response1),4)]      
+    subjects2 = [response2[i] for i in range(0,len(response2),4)]  
     
-    intersection = len(set(objects1).intersection(set(objects2)))
+    intersection = len(set(subjects1).intersection(set(subjects2)))
 
-    union = len(list(set(objects1).union(set(objects2))))
+    union = len(list(set(subjects1).union(set(subjects2))))
     
     if union == 0:
         union = 1
         
     print(union)
     print(intersection)
-
     Jaccard = float(intersection) / union
     return Jaccard
     
-    
-    
+#    length1 = requests.get('https://hdt.lod.labs.vu.nl/triple/count', params=params1)
+#    params1 = params1 + ('page_length',length1)    
+#length2 = requests.get('https://hdt.lod.labs.vu.nl/triple/count', params=params2)       
